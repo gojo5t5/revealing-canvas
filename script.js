80fdc0377257ccf3b2db0d52b5a5d8b84f605ac5
spdskatr <25078279+spdskatr@users.noreply.github.com>
2022-01-24 09:28:47 +0000

    wait for stuff to finish

--- script.js
+++ script.js
@@ -16,40 +16,40 @@ function init() {
     if (!MediaRecorder.isTypeSupported("audio/webm"))
       return alert("Browser not supported");
     mediaRecorder = new MediaRecorder(stream, { mimeType: "audio/webm" });
+
+      const socket = new WebSocket("wss://api.deepgram.com/v1/listen", [
+        "token",
+        apikey,
+      ]);
+
+      socket.onopen = () => {
+        console.log({ event: "onopen" });
+        mediaRecorder.addEventListener("dataavailable", async (event) => {
+          if (event.data.size > 0 && socket.readyState == 1) {
+            socket.send(event.data);
+          }
+        });
+        mediaRecorder.start(100);
+      };
+
+      socket.onmessage = (message) => {
+        console.log({ event: "onmessage", message });
+        const received = JSON.parse(message.data);
+        const transcript = received.channel.alternatives[0].transcript;
+        if (transcript && received.is_final) {
+          let transcript_split = transcript.split(" ")
+          display(transcript_split);
+        }
+      };
+
+      socket.onclose = () => {
+        console.log({ event: "onclose" });
+      };
+
+      socket.onerror = (error) => {
+        console.log({ event: "onerror", error });
+      };
   });
-
-  const socket = new WebSocket("wss://api.deepgram.com/v1/listen", [
-    "token",
-    apikey,
-  ]);
-
-  socket.onopen = () => {
-    console.log({ event: "onopen" });
-    mediaRecorder.addEventListener("dataavailable", async (event) => {
-      if (event.data.size > 0 && socket.readyState == 1) {
-        socket.send(event.data);
-      }
-    });
-    mediaRecorder.start(100);
-  };
-
-  socket.onmessage = (message) => {
-    console.log({ event: "onmessage", message });
-    const received = JSON.parse(message.data);
-    const transcript = received.channel.alternatives[0].transcript;
-    if (transcript && received.is_final) {
-      let transcript_split = transcript.split(" ")
-      display(transcript_split);
-    }
-  };
-
-  socket.onclose = () => {
-    console.log({ event: "onclose" });
-  };
-
-  socket.onerror = (error) => {
-    console.log({ event: "onerror", error });
-  };
 }
 
 function sleep(ms) {
